f65d069b200288040ea513214f14e2c9dc4c4ec1
Wassime Moussaoui <wassime.moussaoui@etudiant.univ-reims.fr>
2025-11-23 15:09:20 +0100

    Creation Table Statistic

---
+++ src/Entity/Statistic.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StatisticRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: StatisticRepository::class)]
+class Statistic
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 40)]
+    private ?string $statisticType = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 5, scale: 2, nullable: true)]
+    private ?string $value = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTime $date = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStatisticType(): ?string
+    {
+        return $this->statisticType;
+    }
+
+    public function setStatisticType(string $statisticType): static
+    {
+        $this->statisticType = $statisticType;
+
+        return $this;
+    }
+
+    public function getValue(): ?string
+    {
+        return $this->value;
+    }
+
+    public function setValue(?string $value): static
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTime
+    {
+        return $this->date;
+    }
+
+    public function setDate(?\DateTime $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+}

39ba992308a2ecfaff538a204a3da9ad82ef2af9
Wassime Moussaoui <wassime.moussaoui@etudiant.univ-reims.fr>
2025-11-23 15:09:10 +0100

    Creation Table Product

---
+++ src/Entity/Product.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $productName = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $productCategory = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getProductName(): ?string
+    {
+        return $this->productName;
+    }
+
+    public function setProductName(string $productName): static
+    {
+        $this->productName = $productName;
+
+        return $this;
+    }
+
+    public function getProductCategory(): ?string
+    {
+        return $this->productCategory;
+    }
+
+    public function setProductCategory(string $productCategory): static
+    {
+        $this->productCategory = $productCategory;
+
+        return $this;
+    }
+}

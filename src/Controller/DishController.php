73bc828007c36a9c1ad72308ba7196be36b503bb
Damien Ho <damien.ho@etudiant.univ-reims.fr>
2025-12-06 19:41:22 +0100

    Ajout de la méthode search() dans DishRepository.php
    + ajout d'une d'une ligne dans .env.test afin de réaliser nos tests sur une bdd sqlite, tel que conseillé classiquement.

--- src/Controller/DishController.php
+++ src/Controller/DishController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
+use App\Repository\DishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class DishController extends AbstractController
 {
     #[Route('/dish', name: 'app_dish')]
-    public function index(): Response
+    public function index(DishRepository $dishes, #[MapQueryParameter] string $search = ''): Response
     {
         return $this->render('dish/index.html.twig', [
-            'controller_name' => 'DishController',
+            'dishes' => $dishes->search($search),
         ]);
     }
 }

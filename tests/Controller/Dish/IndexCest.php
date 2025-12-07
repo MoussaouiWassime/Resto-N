49c0f0468c32fefd8e55e034f19da0ac86d965dd
Damien Ho <damien.ho@etudiant.univ-reims.fr>
2025-12-07 15:30:57 +0100

    Implémentation des dépendances pour les tests Codeception
    + création d'un test pour Index de Dish, encore vide.

---
+++ tests/Controller/Dish/IndexCest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\Tests\Controller\Dish;
+
+use App\\Tests\Support\ControllerTester;
+
+final class IndexCest
+{
+    public function _before(ControllerTester $I): void
+    {
+        // Code here will be executed before each test.
+    }
+
+    public function tryToTest(ControllerTester $I): void
+    {
+        // Write your tests here. All `public` methods will be executed as tests.
+    }
+}

34c64d0e1f901f676af47d87490bc14bf4367d2a
Damien Ho <damien.ho@etudiant.univ-reims.fr>
2025-12-10 17:11:42 +0100

    Ajout du Template show afin de montrer les détails d'un plat.
    - Modification du controller pour la fonction show()
    - Modification de l'index afin d'ajouter la bonne route dans le href.

--- src/Controller/DishController.php
+++ src/Controller/DishController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Dish;
 use App\Repository\DishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,10 +14,21 @@ final class DishController extends AbstractController
     #[Route('/dish', name: 'app_dish')]
     public function index(DishRepository $dishes, #[MapQueryParameter] string $search = ''): Response
     {
-
         return $this->render('dish/index.html.twig', [
             'dishes' => $dishes->search($search),
             'search' => $search,
+        ]);
+    }
+
+    #[Route('/dish/{id}', requirements: ['id' => '\d+'])]
+    public function show(?Dish $dish): Response
+    {
+        if (null == $dish) {
+            throw $this->createNotFoundException('Plat introuvable.');
+        }
+
+        return $this->render('dish/show.html.twig', [
+            'dish' => $dish,
         ]);
     }
 }

063dd30b2403bbd199a35704483e3f8eb7415040
Vigne Lucas <lvge.one@gmail.com>
2025-12-14 16:39:00 +0100

    features : Creation de la méthode show pour la recherche d'un restaurant par ID

--- src/Controller/RestaurantController.php
+++ src/Controller/RestaurantController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Restaurant;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,6 +14,14 @@ final class RestaurantController extends AbstractController
     public function index(): Response
     {
         return $this->render('restaurant/index.html.twig', [
+            'controller_name' => 'RestaurantController',
+        ]);
+    }
+
+    #[Route('/restaurant/{id}', name: 'app_restaurant_show', requirements: ['id' => '\d+'])]
+    public function show(#[MapEntity(expr: 'repository.findWithId(id)')] Restaurant $restaurant): Response
+    {
+        return $this->render('restaurant/show.html.twig', [
             'controller_name' => 'RestaurantController',
         ]);
     }

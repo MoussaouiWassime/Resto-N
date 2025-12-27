7f3527387e36c8011f36f7fc575ad9a98c995ec9
Wassime Moussaoui <wassime.moussaoui@etudiant.univ-reims.fr>
2025-12-27 18:31:00 +0100

    feat: vue de l'acceuil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\RestaurantCategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    #[Route('/', name: 'app_home')]
+    public function index(RestaurantCategoryRepository $categoryRepository): Response
     {
+        $categories = $categoryRepository->findAll();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'categories' => $categories,
         ]);
     }
 }

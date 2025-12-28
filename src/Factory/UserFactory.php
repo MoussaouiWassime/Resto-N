41a9deea3daac30cd24dba5427db8d23c36a274d
Wassime Moussaoui <wassime.moussaoui@etudiant.univ-reims.fr>
2025-12-28 15:27:15 +0100

    génération UserFactory + méthode defaults, initialize et __construct corrigé

---
+++ src/Factory/UserFactory.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+
+/**
+ * @extends PersistentProxyObjectFactory<User>
+ */
+final class UserFactory extends PersistentProxyObjectFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct(
+        private ?UserPasswordHasherInterface $passwordHasher = null,
+    ) {
+        parent::__construct();
+    }
+
+    public static function class(): string
+    {
+        return User::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function defaults(): array|callable
+    {
+        return [
+            'email' => self::faker()->unique()->numerify('user-###@example.com'),            'firstname' => self::faker()->firstName(),
+            'lastName' => self::faker()->lastName(),
+            'firstName' => self::faker()->firstName(),
+            'password' => 'test',
+            'roles' => [],
+            'phone' => substr(self::faker()->phoneNumber(), 0, 20),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): static
+    {
+        return $this
+            ->afterInstantiate(function (User $user) {
+                if (null !== $this->passwordHasher) {
+                    $user->setPassword($this->passwordHasher->hashPassword($user, $user->getPassword()));
+                }
+            })
+        ;
+    }
+}

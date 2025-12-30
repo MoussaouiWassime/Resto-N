a4aea673d55978220d23700a4683604fe10fe561
Wassime Moussaoui <wassime.moussaoui@etudiant.univ-reims.fr>
2025-12-30 16:38:30 +0100

    feat: création RestaurantTableFixtures

---
+++ src/DataFixtures/RestaurantTableFixtures.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Factory\RestaurantTableFactory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class RestaurantTableFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        RestaurantTableFactory::createMany(100);
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            RestaurantFixtures::class,
+        ];
+    }
+}

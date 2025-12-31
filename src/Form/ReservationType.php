ce48fbc012599afd9ac133652bd208a884a2cd6a
Wassime Moussaoui <wassime.moussaoui@etudiant.univ-reims.fr>
2025-12-31 16:38:22 +0100

    fix: nombre minimum et max de personne

--- src/Form/ReservationType.php
+++ src/Form/ReservationType.php
@@ -3,12 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Reservation;
-use App\Entity\Restaurant;
-use App\Entity\RestaurantTable;
-use App\Entity\User;
-use Doctrine\DBAL\Types\DateTimeType;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -23,6 +20,10 @@ class ReservationType extends AbstractType
             ])
             ->add('numberOfPeople', IntegerType::class, [
                 'label' => 'Nombre de personnes',
+                'attr' => [
+                    'min' => 1,
+                    'max' => 10,
+                ],
             ])
         ;
     }

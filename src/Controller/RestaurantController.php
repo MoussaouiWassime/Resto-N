8878386839281112983ebe3c7f8ce3b25012f7ba
vign0057 <lucas.vigne@etudiant.univ-reims.fr>
2026-01-03 20:15:21 +0100

    Configuration : Ajout de classe pour la page index des restaurants

--- src/Controller/RestaurantController.php
+++ src/Controller/RestaurantController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Restaurant;
+use App\Repository\RestaurantRepository;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class RestaurantController extends AbstractController

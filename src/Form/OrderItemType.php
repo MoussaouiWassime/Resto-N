cfcab6f1d51176fb9b36650c4a5a85822e71b9ad
Wassime Moussaoui <wassime.moussaoui@etudiant.univ-reims.fr>
2026-01-05 00:00:03 +0100

    formulaire pour passage de commande

--- src/Form/OrderItemType.php
+++ src/Form/OrderItemType.php
@@ -2,11 +2,9 @@
 
 namespace App\Form;
 
-use App\Entity\Dish;
-use App\Entity\Order;
 use App\Entity\OrderItem;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,14 +13,11 @@ class OrderItemType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('quantity')
-            ->add('order', EntityType::class, [
-                'class' => Order::class,
-                'choice_label' => 'id',
-            ])
-            ->add('dish', EntityType::class, [
-                'class' => Dish::class,
-                'choice_label' => 'id',
+            ->add('quantity', IntegerType::class, [
+                'label' => false,
+                'attr' => [
+                    'min' => 0,
+                ],
             ])
         ;
     }
--- src/Form/OrderType.php
+++ src/Form/OrderType.php
@@ -7,6 +7,9 @@ use App\Entity\Restaurant;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,19 +18,31 @@ class OrderType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('orderDate')
-            ->add('orderType')
-            ->add('status')
-            ->add('deliveryAddress')
-            ->add('deliveryCity')
-            ->add('deliveryPostalCode')
-            ->add('restaurant', EntityType::class, [
-                'class' => Restaurant::class,
-                'choice_label' => 'id',
+            ->add('orderType', ChoiceType::class, [
+                'label' => 'Type de commande',
+                'choices' => [
+                    'Sur place' => 'S',
+                    'A emporter' => 'E',
+                    'Livraison' => 'L',
+                ],
             ])
-            ->add('user', EntityType::class, [
-                'class' => User::class,
-                'choice_label' => 'id',
+            ->add('deliveryAddress', TextType::class, [
+                'label' => 'Adresse (si livraison)',
+                'required' => false,
+            ])
+            ->add('deliveryPostalCode', TextType::class, [
+                'label' => 'Code Postal',
+                'required' => false,
+            ])
+            ->add('deliveryCity', TextType::class, [
+                'label' => 'Ville',
+                'required' => false,
+            ])
+            ->add('orderItems', CollectionType::class, [
+                'entry_type' => OrderItemType::class,
+                'entry_options' => ['label' => false],
+                'by_reference' => false,
+                'label' => false,
             ])
         ;
     }

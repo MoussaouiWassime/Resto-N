390edf284632c5d24c8d9cf91941e07324f2c5dd
Wassime Moussaoui <wassime.moussaoui@etudiant.univ-reims.fr>
2026-01-05 22:46:03 +0100

    update profile

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Form\ProfileType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -15,6 +18,25 @@ final class ProfileController extends AbstractController
     {
         return $this->render('profile/show.html.twig', [
             'user' => $this->getUser(),
+        ]);
+    }
+
+    #[Route('/profile/update', name: 'app_profile_update')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    public function update(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(ProfileType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_profile_show');
+        }
+
+        return $this->render('profile/update.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }

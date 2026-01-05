1e80cd2b2dd1ca357b8be7a83e5862ed35cd125c
Lucas Vigne <lucas.vigne@etudiant.univ-reims.fr>
2026-01-05 11:04:54 +0100

    Correction : Correction syntaxique

--- src/DataFixtures/DishFixtures.php
+++ src/DataFixtures/DishFixtures.php
@@ -16,7 +16,6 @@ class DishFixtures extends Fixture implements DependentFixtureInterface
         // $manager->persist($product);
         $restaurants = RestaurantFactory::all();
         foreach ($restaurants as $restaurant) {
-
             // On crée entre 10 et 20 plats pour ce restaurant
             DishFactory::createMany(10, [
                 'restaurant' => $restaurant,

7c4176bd671aa3d9d0844aeaa2b7531e9cf0b1bc
Lucas Vigne <lucas.vigne@etudiant.univ-reims.fr>
2026-01-05 11:19:04 +0100

    Test : Ajout des test fonctionnels pour la page d'un restaurants.

---
+++ tests/Controller/Restaurant/ShowCest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\Tests\Controller\Restaurant;
+
+use App\Factory\DishFactory;
+use App\Factory\RestaurantFactory;
+use App\Tests\Support\ControllerTester;
+
+final class ShowCest
+{
+    public function restaurantPageContainsRightNumberDishCategories(ControllerTester $I): void
+    {
+        $dishes = DishFactory::createMany(10);
+        RestaurantFactory::createOne(['name' => 'Aaaaamanda', 'dishes' => $dishes]);
+        $I->amOnPage('/restaurant');
+        $I->click('Aaaaamanda');
+        $I->seeResponseCodeIsSuccessful();
+        $I->see('Aaaaamanda', 'h1');
+        $I->seeElement('nav.filtres-dish');
+        $I->seeNumberOfElements('nav.filtres-dish > a', 4);
+    }
+
+    public function restaurantPageContainsDescriptionAdressAndAction(ControllerTester $I): void
+    {
+        $dishes = DishFactory::createMany(10);
+        RestaurantFactory::createOne(['name' => 'Aaaaamanda', 'dishes' => $dishes]);
+        $I->amOnPage('/restaurant');
+        $I->click('Aaaaamanda');
+        $I->amOnPage('/restaurant/1');
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeElement('span.description');
+        $I->seeElement('span.address');
+        $I->seeElement('nav.action-restaurant');
+        $I->seeElement('a.btn');
+    }
+}

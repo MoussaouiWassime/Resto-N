a26b6fe44eb92cb3644a9eb8ca1c1ec38b0d0a2b
Wassime Moussaoui <wassime.moussaoui@etudiant.univ-reims.fr>
2026-01-05 22:36:54 +0100

    formulaire ProfileType

--- src/Form/ProfileType.php
+++ src/Form/ProfileType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,12 +14,19 @@ class ProfileType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('firstName')
-            ->add('lastName')
-            ->add('phone')
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse email',
+            ])
+            ->add('firstName', TextType::class, [
+                'label' => 'Prénom',
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => 'Nom',
+            ])
+            ->add('phone', TextType::class, [
+                'label' => 'Téléphone',
+                'required' => false,
+            ])
         ;
     }
 

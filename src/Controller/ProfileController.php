c56257d65ee9497882d7d2b047b4e1963c5bef8a
Wassime Moussaoui <wassime.moussaoui@etudiant.univ-reims.fr>
2026-01-05 11:29:12 +0100

    page des profils

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -5,14 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
     public function index(): Response
     {
         return $this->render('profile/index.html.twig', [
-            'controller_name' => 'ProfileController',
+            'user' => $this->getUser(),
         ]);
     }
 }

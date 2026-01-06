af319b8b389405b416ce778426500295e28993a6
Wassime Moussaoui <wassime.moussaoui@etudiant.univ-reims.fr>
2026-01-06 16:07:48 +0100

    formulaire d'inscription

--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -4,20 +4,38 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class RegistrationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('firstName')
-            ->add('lastName')
-            ->add('phone')
+            ->add('firstName', TextType::class, [
+                'label' => 'Prénom',
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => 'Nom',
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+            ])
+            ->add('phone', TextType::class, [
+                'label' => 'Téléphone',
+            ])
+            ->add('password', PasswordType::class, [
+                'mapped' => false,
+                'label' => 'Mot de passe',
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new Length(min: 6, max: 4096, minMessage: 'Votre mot de passe doit faire au moins 6 caractères'),
+                ],
+            ])
         ;
     }
 

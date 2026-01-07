461321ea419afc5c03f84e7d0874ee77782fe682
Wassime Moussaoui <wassime.moussaoui@etudiant.univ-reims.fr>
2026-01-07 13:17:47 +0100

    page des stock d'un restaurant

--- src/Controller/StockController.php
+++ src/Controller/StockController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Restaurant;
+use App\Repository\StockRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class StockController extends AbstractController
 {
-    #[Route('/stock', name: 'app_stock')]
-    public function index(): Response
+    #[Route('/restaurant/{id}/stock', name: 'app_stock_index')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    public function index(Restaurant $restaurant, StockRepository $stockRepository): Response
     {
         return $this->render('stock/index.html.twig', [
-            'controller_name' => 'StockController',
+            'restaurant' => $restaurant,
+            'stocks' => $stockRepository->findBy(['restaurant' => $restaurant]),
         ]);
     }
 }

0e8c1d6b5cb8001ba2bc3c44ffe189f30f9a56d5
Wassime Moussaoui <wassime.moussaoui@etudiant.univ-reims.fr>
2026-01-07 09:42:09 +0100

    Formulaire Stock

--- src/Form/StockType.php
+++ src/Form/StockType.php
@@ -7,6 +7,7 @@ use App\Entity\Restaurant;
 use App\Entity\Stock;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,14 +16,9 @@ class StockType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('quantity')
-            ->add('restaurant', EntityType::class, [
-                'class' => Restaurant::class,
-                'choice_label' => 'id',
-            ])
-            ->add('product', EntityType::class, [
-                'class' => Product::class,
-                'choice_label' => 'id',
+            ->add('quantity', IntegerType::class, [
+                'label' => 'Quantité en stock',
+                'attr' => ['min' => 0],
             ])
         ;
     }

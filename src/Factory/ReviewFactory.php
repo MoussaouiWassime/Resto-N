07897ca4fe81c53d7e07e4d1fac081727d50a06c
Wassime Moussaoui <wassime.moussaoui@etudiant.univ-reims.fr>
2026-01-11 16:27:41 +0100

    reviewFactory

---
+++ src/Factory/ReviewFactory.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Review;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+
+/**
+ * @extends PersistentProxyObjectFactory<Review>
+ */
+final class ReviewFactory extends PersistentProxyObjectFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+    }
+
+    public static function class(): string
+    {
+        return Review::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function defaults(): array|callable
+    {
+        return [
+            'createdAt' => \DateTimeImmutable::createFromMutable(self::faker()->dateTime()),
+            'restaurant' => RestaurantFactory::new(),
+            'review' => self::faker()->randomNumber(),
+            'user' => UserFactory::new(),
+            'comment' => self::faker()->text(255),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): static
+    {
+        return $this
+            // ->afterInstantiate(function(Review $review): void {})
+        ;
+    }
+}

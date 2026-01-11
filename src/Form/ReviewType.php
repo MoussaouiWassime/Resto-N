b41a785db987d6ce3e1a11d5e330e8e003d4da39
Wassime Moussaoui <wassime.moussaoui@etudiant.univ-reims.fr>
2026-01-11 18:53:01 +0100

    reviewType

--- src/Form/ReviewType.php
+++ src/Form/ReviewType.php
@@ -2,11 +2,11 @@
 
 namespace App\Form;
 
-use App\Entity\Restaurant;
 use App\Entity\Review;
-use App\Entity\User;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,18 +15,29 @@ class ReviewType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('review')
-            ->add('comment')
-            ->add('createdAt', null, [
-                'widget' => 'single_text',
+            ->add('rating', ChoiceType::class, [
+                'label' => 'Note globale',
+                'choices' => [
+                    'Excellent (5/5)' => 5,
+                    'Très bon (4/5)' => 4,
+                    'Moyen (3/5)' => 3,
+                    'Décevant (2/5)' => 2,
+                    'Mauvais (1/5)' => 1,
+                ],
+                'expanded' => true,
+                'multiple' => false,
             ])
-            ->add('user', EntityType::class, [
-                'class' => User::class,
-                'choice_label' => 'id',
+            ->add('comment', TextareaType::class, [
+                'label' => 'Votre avis',
+                'required' => false,
+                'attr' => [
+                    'placeholder' => 'Racontez votre expérience (max 255 caractères)...',
+                    'maxlength' => 255,
+                ],
             ])
-            ->add('restaurant', EntityType::class, [
-                'class' => Restaurant::class,
-                'choice_label' => 'id',
+            ->add('submit', SubmitType::class, [
+                'label' => 'Publier mon avis',
+                'attr' => ['class' => 'btn btn-primary'],
             ])
         ;
     }

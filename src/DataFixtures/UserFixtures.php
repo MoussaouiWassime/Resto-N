e59b8956197c9302ec71ab24add005dc4e59f82d
Damien Ho <damien.ho@etudiant.univ-reims.fr>
2026-01-12 02:21:03 +0100

    Ajout de deux utilisateurs (ROLE_ADMIN)

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -10,9 +10,20 @@ class UserFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        UserFactory::createOne([
+            'firstname' => 'Damien',
+            'lastname' => 'Ho',
+            'email' => 'root@example.com',
+            'roles' => ['ROLE_ADMIN'],
+        ]);
 
-        UserFactory::createMany(5);
+        UserFactory::createOne([
+            'firstname' => 'Wassime',
+            'lastname' => 'Moussaoui',
+            'email' => 'user@example.com',
+            'roles' => ['ROLE_ADMIN'],
+        ]);
+
+        UserFactory::createMany(10);
     }
 }

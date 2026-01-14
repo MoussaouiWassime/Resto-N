8ab4ab1c4d047a836d79de13a8f8e16958548632
Vigne Lucas <lvge.one@gmail.com>
2026-01-14 02:27:22 +0100

    Configuration : Ajout d'import de classe necéssaire pour la création, la modification et la suppression de plat.

--- src/Controller/DishController.php
+++ src/Controller/DishController.php
@@ -3,11 +3,18 @@
 namespace App\Controller;
 
 use App\Entity\Dish;
+use App\Entity\Restaurant;
+use App\Form\DishType;
 use App\Repository\DishRepository;
+use App\Repository\RoleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class DishController extends AbstractController
 {

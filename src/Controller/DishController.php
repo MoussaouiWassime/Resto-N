881581e099736ede594821b08e20667800ef0fb6
Wassime Moussaoui <wassime.moussaoui@etudiant.univ-reims.fr>
2026-01-18 12:34:36 +0100

    ajout message confirmation modification plat

--- src/Controller/DishController.php
+++ src/Controller/DishController.php
@@ -79,6 +79,8 @@ final class DishController extends AbstractController
             $entityManager->persist($dish);
             $entityManager->flush();
 
+            $this->addFlash('success', 'Le plat a bien été ajouté à la carte !');
+
             return $this->redirectToRoute('app_restaurant_show', [
                 'id' => $restaurant->getId(),
             ], 307);
@@ -128,6 +130,7 @@ final class DishController extends AbstractController
                 $dish->setPhoto($newFileName);
             }
             $entityManager->flush();
+            $this->addFlash('success', 'Le plat a été modifié avec succès.');
 
             return $this->redirectToRoute('app_restaurant_show', [
                 'id' => $restaurant->getId(),

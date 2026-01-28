f59196e3fc64cbf25fa7c32f96517254c19bc559
Wassime Moussaoui <wassime.moussaoui@etudiant.univ-reims.fr>
2026-01-28 13:53:48 +0100

    refactor: update ReviewFixtures to use dynamic user selection and modify restaurant details

--- src/DataFixtures/ReviewFixtures.php
+++ src/DataFixtures/ReviewFixtures.php
@@ -16,41 +16,32 @@ class ReviewFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $restaurant = RestaurantFactory::createOne([
-            'name' => 'Restaurant Cutrona',
+            'name' => 'Restaurant Gourmet',
             'description' => 'Le meilleur Restaurant de toute la France! Si vous cherchez
-                une ambiance conviviale, couplée avec de bons petits plats, venez manger chez Cutrona!',
+                une ambiance conviviale, couplée avec de bons petits plats, venez manger chez nous!',
             'address' => 'Chemin du Roulier',
             'postal_code' => '51100',
             'city' => 'Reims',
-            'image' => 'https://iut-info.univ-reims.fr/users/cutrona/intranet/css/images/jerome-cutrona.jpg',
+            'image' => 'https://example.com/restaurant.jpg',
             'categories' => RestaurantCategoryFactory::randomRange(1, 2),
         ]);
 
-        $cutrona = UserFactory::findBy(['lastName' => 'Cutrona']);
-        $damien = UserFactory::findBy(['lastName' => 'Ho']);
-        $wassime = UserFactory::findBy(['lastName' => 'Moussaoui']);
+        $users = UserFactory::randomRange(3, 3);
 
         RoleFactory::createOne([
             'restaurant' => $restaurant,
-            'user' => $cutrona[0],
+            'user' => $users[0],
             'role' => RestaurantRole::OWNER,
         ]);
         RoleFactory::createOne([
             'restaurant' => $restaurant,
-            'user' => $damien[0],
+            'user' => $users[1],
             'role' => RestaurantRole::SERVER,
         ]);
-        RoleFactory::createOne([
-            'restaurant' => $restaurant,
-            'user' => $wassime[0],
-            'role' => RestaurantRole::SERVER,
-        ]);
-
-
 
         ReviewFactory::createOne([
             'restaurant' => $restaurant,
-            'user' => $damien[0],
+            'user' => $users[2],
             'rating' => 4,
             'comment' => "C'était très bon, je me suis régalé!",
         ]);
